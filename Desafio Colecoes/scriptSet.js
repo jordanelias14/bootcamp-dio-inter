541f4e2d3fe84e8f8b175319f6348b8dc00d29a0
Jordan <danelias180@gmail.com>
2022-01-12 12:27:41 -0300

    ''

--- Desafio Colecoes/scriptSet.js
+++
@@ -1,9 +0,0 @@
-const meuArr = [5, 6, 5, 7, 5, 50, 5, 55, 50, 100, 32, 33, 32];
-
-function valoresUnicos(arr) {
-  const set = new Set(arr);
-
-  return [...set]; //cada elemento do set adiciona no novo array
-}
-
-console.log(valoresUnicos(meuArr));

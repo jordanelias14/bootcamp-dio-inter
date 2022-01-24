17589d4e7d6a94a30ca6103647c286c4ffb0fec9
Jordan <danelias180@gmail.com>
2022-01-24 18:42:20 -0300

    Desafio DOM Dark and Light Modes

---
+++ Desafio DOM/assets/script.js
@@ -0,0 +1,31 @@
+const button = document.getElementById("mode-selector");
+const h1 = document.getElementById("page-title");
+const body = document.getElementsByTagName("body")[0];
+const footer = document.getElementsByTagName("footer")[0];
+
+function changeMode() {
+  changeClasses();
+  changeText();
+}
+
+function changeClasses() {
+  button.classList.toggle("dark-mode");
+
+  h1.classList.toggle("dark-mode");
+
+  body.classList.toggle("dark-mode");
+
+  footer.classList.toggle("dark-mode");
+}
+
+function changeText() {
+  if (button.classList.contains("dark-mode")) {
+    button.innerHTML = "Light Mode";
+    h1.innerHTML = "Dark Mode" + " ON";
+  } else {
+    button.innerHTML = "Light Mode";
+    h1.innerHTML = "Light Mode" + " ON";
+  }
+}
+
+button.addEventListener("click", changeMode);
